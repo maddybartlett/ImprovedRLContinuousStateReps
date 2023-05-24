63cf6829c9c1a1185dd9d4d81f649952bd7cb7a8
kpc-simone <kpc.simone@gmail.com>
2023-05-24 14:40:44 -0400

    fixed import bug

--- network/rlnet/representations/__init__.py
+++ network/rlnet/representations/__init__.py
@@ -1,6 +1,6 @@
 _all__ = ["normal", "onehot", "ssp","vsa"]
 from .normal import NormalRep
-from .onehot import OneHotRep
+from .onehot import OneHotRepRB, OneHotRepCP
 from .ssp import SSPRep
 from .vsa import VSARep
 from .onehottransform import OneHotRepTransformND
