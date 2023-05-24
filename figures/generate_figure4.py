f34f51d44111de40c317502e705070824de8cf59
kpc-simone <kpc.simone@gmail.com>
2023-05-24 15:43:43 -0400

    organized figure-generating scripts

---
+++ figures/generate_figure4.py
@@ -0,0 +1,185 @@
+### Visualise and Explore Data 
+
+import matplotlib.pyplot as plt
+from matplotlib.legend_handler import HandlerTuple
+import matplotlib.lines as mlines
+import numpy as np
+import pandas as pd
+from scipy import stats
+from scipy.stats import iqr
+import seaborn as sns
+
+# Load dataframes
+data_location = '..//ratboxData//ratbox100_hexssp_x10.csv'
+df_hex100 = pd.read_csv(data_location)
+
+data_location = '..//ratboxData//ratbox50_hexssp_x10.csv'
+df_hex50 = pd.read_csv(data_location)
+
+data_location = '..//ratboxData//ratbox100_discrete6_x10.csv'
+df_discrete6100 = pd.read_csv(data_location)
+
+data_location = '..//ratboxData//ratbox50_discrete6_x10.csv'
+df_discrete650 = pd.read_csv(data_location)
+
+data_location = '..//ratboxData//ratbox100_discrete8_x10.csv'
+df_discrete8100 = pd.read_csv(data_location)
+
+data_location = '..//ratboxData//ratbox50_discrete8_x10.csv'
+df_discrete850 = pd.read_csv(data_location)
+
+data_location = '..//ratboxData//ratbox100_discrete10_x10.csv'
+df_discrete10100 = pd.read_csv(data_location)
+
+data_location = '..//ratboxData//ratbox50_discrete10_x10.csv'
+df_discrete1050 = pd.read_csv(data_location)
+
+data_location = '..//ratboxData//ratbox100_discrete12_x10.csv'
+df_discrete12100 = pd.read_csv(data_location)
+
+data_location = '..//ratboxData//ratbox50_discrete12_x10.csv'
+df_discrete1250 = pd.read_csv(data_location)
+
+
+# Convert relevant data from string to array inside dataframes
+DATAFRAMES = [df_hex100, df_hex50, df_discrete6100, df_discrete650, df_discrete8100, 
+             df_discrete850, df_discrete10100, df_discrete1050, df_discrete12100,
+             df_discrete1250]
+
+for df in DATAFRAMES:
+    for i in range(10):
+        str_array = df['roll_mean'][i].replace("]", "")
+        str_array = str_array.replace("[", "")
+        str_array = str_array.replace("\n", " ")
+        df['roll_mean'][i] = np.fromstring(str_array, sep=' ')
+        
+        str_array = df['episodes'][i].replace('\n', "")
+        str_array = str_array.replace('[', "")
+        str_array = str_array.replace(']', "")
+        df['episodes'][i] = np.fromstring(str_array, sep=' ')
+
+
+# Set formatting values
+axisLabelSize=16
+titleSize=20
+tickSize=14
+legendtitleSize=14
+legendSize=12
+
+COLORS = ['#ffdcc0', '#eaef98', '#97e074', '#54d078', '#37c1b4', '#1e64b1',
+          '#1e08a2', '#6e0092', '#820052', '#730002']
+
+
+### Compare Performance with HexSSP vs Discrete Representation
+mean_rollmean_discrete6100 = np.vstack(df_discrete6100['roll_mean'])[:,99:].mean(axis=0)
+x = np.vstack(df_discrete6100['roll_mean'])[:,99:]
+idr_90_discrete6100 = np.percentile(x, 90, axis=0)
+idr_10_discrete6100 = np.percentile(x, 10, axis=0)
+
+mean_rollmean_discrete8100 = np.vstack(df_discrete8100['roll_mean'])[:,99:].mean(axis=0)
+x = np.vstack(df_discrete8100['roll_mean'])[:,99:]
+idr_90_discrete8100 = np.percentile(x, 90, axis=0)
+idr_10_discrete8100 = np.percentile(x, 10, axis=0)
+
+mean_rollmean_discrete10100 = np.vstack(df_discrete10100['roll_mean'])[:,99:].mean(axis=0)
+x = np.vstack(df_discrete10100['roll_mean'])[:,99:]
+idr_90_discrete10100 = np.percentile(x, 90, axis=0)
+idr_10_discrete10100 = np.percentile(x, 10, axis=0)
+
+mean_rollmean_discrete12100 = np.vstack(df_discrete12100['roll_mean'])[:,99:].mean(axis=0)
+x = np.vstack(df_discrete12100['roll_mean'])[:,99:]
+idr_90_discrete12100 = np.percentile(x, 90, axis=0)
+idr_10_discrete12100 = np.percentile(x, 10, axis=0)
+
+mean_rollmean_hex100 = np.vstack(df_hex100['roll_mean'])[:,99:].mean(axis=0)
+x = np.vstack(df_hex100['roll_mean'])[:,99:]
+idr_90_hex100 = np.percentile(x, 90, axis=0)
+idr_10_hex100 = np.percentile(x, 10, axis=0)
+
+figsize_ = (8,4.3)
+
+fig,(ax1,ax2) = plt.subplots(1,2,figsize = figsize_)
+
+ax1.plot(mean_rollmean_discrete6100, label='6 bins', color=COLORS[0], linewidth=2.5)
+ax1.fill_between(np.arange(0,401), idr_90_discrete6100, idr_10_discrete6100, alpha=0.3, color=COLORS[0])
+ax1.plot(mean_rollmean_discrete8100, label='8 bins', color=COLORS[2], linewidth=2.5)
+ax1.fill_between(np.arange(0,401), idr_90_discrete8100, idr_10_discrete8100, alpha=0.3, color=COLORS[2])
+ax1.plot(mean_rollmean_discrete10100, label='10 bins', color=COLORS[4], linewidth=2.5)
+ax1.fill_between(np.arange(0,401), idr_90_discrete10100, idr_10_discrete10100, alpha=0.3, color=COLORS[4])
+ax1.plot(mean_rollmean_discrete12100, label='12 bins', color=COLORS[6], linewidth=2.5)
+ax1.fill_between(np.arange(0,401), idr_90_discrete12100, idr_10_discrete12100, alpha=0.3, color=COLORS[6])
+ax1.plot(mean_rollmean_hex100, label='Hex SSP', color=COLORS[8], linewidth=2.5)
+ax1.fill_between(np.arange(0,401), idr_90_hex100, idr_10_hex100, alpha=0.3, color=COLORS[8])
+
+ax1.set_ylabel('Average Rolling Mean Reward', fontsize=axisLabelSize)
+ax1.set_xlabel('Episode', fontsize=axisLabelSize)
+ax1.tick_params(labelsize=tickSize)
+
+#plt.legend(bbox_to_anchor=(1.25, 0.0),title='Representation', title_fontsize=legendtitleSize, 
+           #fontsize=legendSize, loc='lower right')
+ax1.legend(loc = 'upper center',bbox_to_anchor=(0.5,1.25),ncol=3,frameon=False)
+ax1.set_xlim(-10, 405)
+
+### Explore How Changing the Agent's Speed Affected Performance
+
+# Create combined dataframes for each speed condition
+term_rew =[np.vstack(df_discrete6100['episodes']).T[-1], 
+           np.vstack(df_discrete8100['episodes']).T[-1], np.vstack(df_discrete10100['episodes']).T[-1], 
+           np.vstack(df_discrete12100['episodes']).T[-1],
+           np.vstack(df_discrete650['episodes']).T[-1], 
+           np.vstack(df_discrete850['episodes']).T[-1], np.vstack(df_discrete1050['episodes']).T[-1], 
+           np.vstack(df_discrete1250['episodes']).T[-1]]
+
+rep = [['6']*10, ['8']*10, ['10']*10, ['12']*10, ['6']*10, ['8']*10, ['10']*10, ['12']*10]
+speed = [['10,000']*40, ['5,000']*40]
+
+combined_all = pd.DataFrame({'terminal reward': np.hstack(term_rew),
+                            'representation': np.hstack(rep),
+                            'pixels per sec': np.hstack(speed)})
+
+
+# Plot the results as a point plot with confidence intervals as error bars
+# fig, ax1 = plt.subplots(1, 1, figsize=(5,4))
+
+sns.set_style('white')
+sns.pointplot(data=combined_all, x='representation', y='terminal reward', hue="pixels per sec", 
+              ci=95, palette=[COLORS[8], COLORS[5]], join=False)
+sns.despine()
+
+mu_hex100 = np.vstack(df_hex100['episodes']).mean(axis=0)[-1]
+sigma_hex100 = np.vstack(df_hex100['episodes']).std(axis=0)[-1]
+ci_hex100 = stats.norm.interval(0.95, loc=mu_hex100, scale=sigma_hex100)
+
+mu_hex50 = np.vstack(df_hex50['episodes']).mean(axis=0)[-1]
+sigma_hex50 = np.vstack(df_hex50['episodes']).std(axis=0)[-1]
+ci_hex50 = stats.norm.interval(0.95, loc=mu_hex50, scale=sigma_hex50)
+
+l1 = ax2.axhline(y=mu_hex100, color=COLORS[8], linestyle='-', linewidth=2.5)
+plt.fill_between(np.arange(-.5,4.5), ci_hex100[0], ci_hex100[1], alpha=0.3, color=COLORS[8])
+
+l2 = ax2.axhline(y=mu_hex50, color=COLORS[5], linestyle='-', linewidth=2.5)
+plt.fill_between(np.arange(-.5,4.5), ci_hex50[0], ci_hex50[1], alpha=0.3, color=COLORS[5])
+
+ax2.set_ylabel('Terminal Reward', fontsize=axisLabelSize)
+ax2.set_xlabel('Discretization (# bins per state)', fontsize=axisLabelSize)
+
+ax2.tick_params(labelsize=tickSize)
+
+m1 = mlines.Line2D([], [], color=COLORS[8], marker='.', linestyle='None',
+                          markersize=9, label='10,000 pix/sec')
+m2 = mlines.Line2D([], [], color=COLORS[5], marker='.', linestyle='None',
+                          markersize=9, label='5,000 pix/sec')
+l1 = mlines.Line2D([], [], color=COLORS[8], marker='.', linestyle='-',
+                          markersize=1, label='HexSSP')
+l2 = mlines.Line2D([], [], color=COLORS[5], marker='.', linestyle='-',
+                          markersize=1, label='HexSSP')
+
+ax2.legend(handles=[m1, m2, (l1,l2)], labels=['10,000 pix/sec', '5,000 pix/sec', 'HexSSP'], 
+          fontsize=legendSize, handler_map={tuple: HandlerTuple(ndivide=None)},
+          bbox_to_anchor=(0.9, 1.2), ncol=2, frameon=False,)
+
+ax2.set_xlim(-0.5, 3.5)
+
+fig.tight_layout()
+fig.savefig("figure4_ratbox_discrete_vs_hex.pdf", bbox_inches="tight")
+plt.show()
--- figures/generate_figure6.py
+++ figures/generate_figure6.py
@@ -7,11 +7,11 @@ from scipy.stats import levene
 window = 50
 
 # Grab all files in subfolder HexPlace (this will contain reward.csv, reward(1).csv, etc)
-ssp_files = [f for f in listdir("./HexPlace")]
+ssp_files = [f for f in listdir("../cartpoleData/baseline-comparison/HexSSP")]
 all_mean_rews = []
 all_ts = []
 for i, name in enumerate(ssp_files):
-    ssp_data = np.genfromtxt("./HexPlace/" + name, delimiter=',')[1:]
+    ssp_data = np.genfromtxt("../cartpoleData/baseline-comparison/HexSSP/" + name, delimiter=',')[1:]
     ssp_data[np.isnan(ssp_data)] = 0
     # Epsiodic rewards
     ep_rews = np.sum(ssp_data[:,1:],axis=0)
@@ -38,9 +38,16 @@ ssp_min = np.percentile(ssp_int_rews, 10, axis=0, interpolation = 'midpoint')
 ssp_max =  np.percentile(ssp_int_rews, 90, axis=0, interpolation = 'midpoint')
 
 # The baseline data
-baseline_ts = np.load('../cartpoleData/baseline_num_timesteps.npy', allow_pickle=True)
-baseline_rs = np.load('../cartpoleData/baseline_rewards.npy', allow_pickle=True)
+baseline_ts = np.load('../cartpoleData/baseline-comparison/baseline/baseline_num_timesteps.npy', allow_pickle=True)
+baseline_rs = np.load('../cartpoleData/baseline-comparison/baseline/baseline_rewards.npy', allow_pickle=True)
 
+# I ran 20 extra trials and saved with a different filename, if the 
+# cartpolae_a2c.py is simply run with 20 trials this code is not needed
+
+baseline2_ts = np.load('../cartpoleData/baseline-comparison/baseline/baseline_num_timesteps_set2.npy', allow_pickle=True)
+baseline2_rs = np.load('../cartpoleData/baseline-comparison/baseline/baseline_rewards_set2.npy', allow_pickle=True)
+baseline_ts= np.concatenate((baseline_ts,baseline2_ts))
+baseline_rs= np.concatenate((baseline_rs,baseline2_rs))
 
 # Again usnig interpolate  due to unequal sampling 
 bl_ts = np.arange(np.max([np.min(t) for t in baseline_ts]), np.min([np.max(t) for t in baseline_ts]), 50)
@@ -55,21 +62,27 @@ bl_min = np.percentile(bl_int_rews, 10, axis=0, interpolation = 'midpoint')
 bl_max = np.percentile(bl_int_rews, 90, axis=0, interpolation = 'midpoint')
 
 
-fig=plt.figure(figsize=(3.37566, 2))
-plt.plot(bl_ts, bl_mean, c="red", linewidth=1.5, label='Baseline A2C')
-plt.fill_between(bl_ts, bl_min, bl_max, color="red", alpha=.15)
+fig,ax = plt.subplots(1,1,figsize=(3.5, 2.5))
+ax.plot(bl_ts, bl_mean, c="red", linewidth=1.5, label='Baseline A2C')
+ax.fill_between(bl_ts, bl_min, bl_max, color="red", alpha=.15)
 
-plt.plot(ssp_ts, ssp_mean, c="blue", linewidth=1.5, label='HexSSP')
-plt.fill_between(ssp_ts, ssp_min, ssp_max, color="blue", alpha=.15)
+ax.plot(ssp_ts, ssp_mean, c="blue", linewidth=1.5, label='HexSSP')
+ax.fill_between(ssp_ts, ssp_min, ssp_max, color="blue", alpha=.15)
 
-plt.xlabel('Number of Timesteps')
-plt.ylabel('Episodic Reward')
-plt.legend(loc='lower right')
-plt.xlim([0,bl_ts[-1]])
-utils.save(fig,'cartpole_ssp_vs_baseline.pdf')
+ax.set_xlabel('Number of Timesteps')
+ax.set_ylabel('Episodic Reward')
+ax.legend(loc='lower right')
+ax.set_xlim([0,bl_ts[-1]])
+ax.set_xticks([0,50000,100000,150000])
 
+for spine in ['top','right']:
+    ax.spines[spine].set_visible(False)
 
+fig.tight_layout()
+plt.savefig('figure6_cartpole_ssp_vs_baseline.pdf')
+plt.show()
 
+### Investigate
 ssp_end_vals = [all_mean_rews[i][np.where(all_ts[i]> bl_ts[-1])[0][0]] for i in range(len(all_ts))]
 bl_end_vals = [baseline_rs[i][np.where(baseline_ts[i]> bl_ts[-1])[0][0]] for i in range(len(baseline_rs))]
 print(np.var(ssp_end_vals))
